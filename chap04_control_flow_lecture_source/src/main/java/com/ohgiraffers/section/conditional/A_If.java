8cfa371e958a8e533320cda07c45121c0866da65
HEun0420 <usialeta@gmail.com>
2024-05-17 17:23:46 +0900

    swich

--- chap04_control_flow_lecture_source/src/main/java/com/ohgiraffers/section/conditional/A_If.java
+++ chap04_control_flow_lecture_source/src/main/java/com/ohgiraffers/section/conditional/A_If.java
@@ -6,7 +6,7 @@ public class A_If {
 
     /*
     * [if문 표현식]
-    * ir([조건식]) {
+    * if([조건식]) {
     *       // 조건식이 true일 때 실행할 명령문
     * }
      */
@@ -18,26 +18,26 @@ public class A_If {
      */
 
 
-//    // 단일 if 문 흐름 확인
-//    public void testSimpleIfStatement() {
-//
-//        // 정수 한개를 입력 받아
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("숫자 한개를 입력하시오");
-//        int num = sc.nextInt();
-//
-//        // 그 수가 짝수이면 "입력하신 숫자는 짝수입니다." 라고 출력
-//        // 짝수가 아니면 출력하지 않는 코드를 작성한다.
-//        if(num % 2 == 0) {
-//            System.out.println("입력하신 숫자는 짝수입니다.");
-//        }
-//
-//        // 단, 조건과 상관없이 프로그램이 종료될 때 " 프로그램을 종료합니다" 라고 출력
-//        System.out.println("프로그램을 종료합니다");
-//
-//
-//    }
+    // 단일 if 문 흐름 확인
+    public void testSimpleIfStatement() {
 
+        // 정수 한개를 입력 받아
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자 한개를 입력하시오");
+        int num = sc.nextInt();
+
+        // 그 수가 짝수이면 "입력하신 숫자는 짝수입니다." 라고 출력
+        // 짝수가 아니면 출력하지 않는 코드를 작성한다.
+        if(num % 2 == 0) {
+            System.out.println("입력하신 숫자는 짝수입니다.");
+        }
+
+        // 단, 조건과 상관없이 프로그램이 종료될 때 " 프로그램을 종료합니다" 라고 출력
+        System.out.println("프로그램을 종료합니다");
+
+
+   }
+//
 //        // 중첩된 if 문 실행 흐름 확인
 //    public void testNestedIfStatment() {
 //        // 정수 한개를 입력받아
@@ -57,39 +57,35 @@ public class A_If {
 //        // 둘 중 하나라도 해당하지 않으면 아무 내용도 출력하지 않는 코드를 작성해보자.
 //        // 단, 조건과 상관없이 프로그램이 종료될 때 "프로그램을 종료합니다." 라고 출력한다.
 //        System.out.println("프로그램을 종료합니다");
-//
-//
+
+
 //    }
-
-    // 중첩된 if-else 문 실행 흐름 확인
-    public void testNestedIfStatment() {
-
-        // 숫자를 하나 입력 받아
-        Scanner sc = new Scanner(System.in);
-        System.out.println("정수를 하나 입력하세요 : ");
-        int num = sc.nextInt();
-
-        //양수이면 "입력하신 숫자는 양수입니다." 출력하고
-        // 음수이면 "입력하신 숫자는 음수 입니다." 출력하고
-        if(num!=0){
-            if(num>0){
-                System.out.println("입력하신 숫자는 양수입니다.");
-            } else {
-                System.out.println("입력하신 숫자는 음수 입니다.");
-            }
-            // 단, 0 이면 "0 입니다"라고 출력하세요
-        } else{
-                System.out.println("0 입니다.");
-            }
-
-
-
-
-        // 조건과 상관없이 프로그램이 종료될 때 "프로그램을 종료합니다"라고 출력되도록 한다.
+//
+//    // 중첩된 if-else 문 실행 흐름 확인
+//    public void testNestedIfStatment() {
+//
+//        // 숫자를 하나 입력 받아
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("정수를 하나 입력하세요 : ");
+//        int num = sc.nextInt();
+//
+//        //양수이면 "입력하신 숫자는 양수입니다." 출력하고
+//        // 음수이면 "입력하신 숫자는 음수 입니다." 출력하고
+//        if(num!=0){
+//            if(num>0){
+//                System.out.println("입력하신 숫자는 양수입니다.");
+//            } else {
+//                System.out.println("입력하신 숫자는 음수 입니다.");
+//            }
+//            // 단, 0 이면 "0 입니다"라고 출력하세요
+//        } else{
+//                System.out.println("0 입니다.");
+//            }
+//        // 조건과 상관없이 프로그램이 종료될 때 "프로그램을 종료합니다"라고 출력되도록 한다.
+//        System.out.println("프로그램을 종료합니다.");
 
 
 
-
-    }
+//    }
 
 }
--- chap04_control_flow_lecture_source/src/main/java/com/ohgiraffers/section/conditional/Application1.java
+++ chap04_control_flow_lecture_source/src/main/java/com/ohgiraffers/section/conditional/Application1.java
@@ -4,11 +4,11 @@ public class Application1 {
     // 실행용 클래스
     public static void main(String[] args) {
 
-        A_If aIf = new A_If();
-
+//        A_If aIf = new A_If();
+//
 //        aIf.testSimpleIfStatement(); //단일 if문
-   //     aIf.testNestedIfStatment(); // 만일 아직 만들지 않는 메서드를 데려오려면 프로그램 이름을 정하고=>
-                                    // 그 위에 마우스를 올려놓고 alt + enter하면 A_if에 명령문 틀이 만들어진다.
+//        aIf.testNestedIfStatment(); // 만일 아직 만들지 않는 메서드를 데려오려면 프로그램 이름을 정하고=>
+//                                     그 위에 마우스를 올려놓고 alt + enter하면 A_if에 명령문 틀이 만들어진다.
 
 
 
@@ -25,7 +25,8 @@ public class Application1 {
 //          dSwitch.testSimpleSwitchStatement();
 
         D_Swich dSwitch= new D_Swich();
-        dSwitch.testSwitchVendingMachine();
+//        dSwitch.testSwitchVendingMachine();
+        dSwitch.testSwitchstory();
 
 
     }
