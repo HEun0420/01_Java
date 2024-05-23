2be28ccdd7eaeb6ab54b786fd5e423b303c8592c
HEun0420 <usialeta@gmail.com>
2024-05-23 14:38:54 +0900

    class

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section2/problem4/Monster.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section2/problem4/Monster.java
@@ -1,4 +1,22 @@
 package com.ohgiraffers.section2.problem4;
 
 public class Monster {
+
+    private String kinds;
+    int hp;
+
+    public void setKinds(String kinds){
+        this.kinds = kinds;
+    }
+
+    public void  setHp(int hp){
+        if(hp>0){
+            this.hp = hp;
+        } else {
+            this.hp = 0;
+        }
+    }
+    public String getInfo() {
+        return " 몬스턴의 종류는 " + kinds + "이고, hp는 " + hp +"이다.";
+    }
 }
