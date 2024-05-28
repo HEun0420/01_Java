2abfe60429f2dff83c1d1f553b043a6ee5b282f0
HEun0420 <usialeta@gmail.com>
2024-05-28 12:20:37 +0900

    오버라이딩

--- chap08-Inheritance-lecture-source/src/main/java/com/ohgiraffers/section3/overriding/SubClass.java
+++ chap08-Inheritance-lecture-source/src/main/java/com/ohgiraffers/section3/overriding/SubClass.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.section3.overriding;
 
-public class SubClass {
+public class SubClass extends SuperClass {
+    // 1. 메소드 이름을 변경하면 에러 발생
+//    @Override
+//    public void method2(int num){}
+
+    //2. 메소드의 리턴타입 변경하면 에러 발생
+//    @Override
+//    public int method(int num) {
+//        return num;
+//    }
+
+    // 3. 매개변수 갯수나, 타입이 변경되면 에러 발생
+//    @Override
+//    public void method(String name){} // 타입 변경
+//    public void method(int num, int num2) {} // 갯수 변경
+
+
+
+
+
+
 }
